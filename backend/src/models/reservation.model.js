1ea278531aad92cf91215cf9af5fcec11ef04887
salam <chaudharysm00@gmail.com>
2025-02-15 19:39:28 +0545

    reservation model

---
+++ backend/src/models/reservation.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const reservationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    person: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Confirmed"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Reservation = mongoose.model("Reservation", reservationSchema);
+export default Reservation;
